e94c75d094878f52d48df3ccd3daaf631f430a6a
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 17:25:28 +0000

    fol map update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,6 @@ st.map(bcen1, color="#ffaa0088", size=10.0)
 
 st.divider()
 
-map = folium.Map(location=[48, -120], zoom_start=4)
+map = folium.Map(location=[55, -123], zoom_start=2)
 st_map = st_folium(map, width=700, height=450)
 
