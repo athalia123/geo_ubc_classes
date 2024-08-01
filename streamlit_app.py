ccee7cf1c78bc68a8bd220c63ed11e3d4b779133
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 16:38:24 +0000

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ st.write("I'm going to make a map of the campus and put my classes on it")
 gclss = gpd.read_file("geo_files/geoclass1.geojson")
 gc = pd.DataFrame(gclss)
 gc1 = gc.drop('geometry', axis=1)
-gc1 = gc1[['Section'],['Instructional Format'],['Days'],['Start'],['End'],['Room'],['Building'],['SHORTNAME']]
-st.dataframe(gc1)
+gc2 = gc1[["Section", "Instructional Format", "Days", "Start", "End", "Room", "Building", "SHORTNAME"]]
+st.dataframe(gc2)
