58033f973cd5f9a788c4dffa248a0d33d025bc12
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 16:55:54 +0000

    app bcen map trial update

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,11 @@ gc = pd.DataFrame(gclss)
 gc1 = gc.drop('geometry', axis=1)
 gc2 = gc1[["Section", "Instructional Format", "Days", "Start", "End", "Room", "Building", "SHORTNAME"]]
 st.dataframe(gc2)
+
+st.divider()
+
+bcen = pd.DataFrame(gpd.read_file("geo_files/ubc_buildings_centroids.geojson"))
+bcen['lon'] = bcen.point_object.x
+bcen['lat'] = bcen.point_object.y
+bcen1 = bcen.drop('geometry', axis=1)
+st.map(bcen1, color="#ffaa0088")
