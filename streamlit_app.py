d9469366c41673dfff084c76dea82afd97498619
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 15:25:42 +0000

    added table gclss data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import geopandas as gpd
+import pandas as pd
 
 st.title('My First App - Hello World :)')
 st.write("I'm going to make a map of the campus and put my classes on it")
+
+gclss = gpd.read_file("geo_files/geoclass1.geojson")
+gc = pd.DataFrame(gclss)
+
+st.table(gc)
 
