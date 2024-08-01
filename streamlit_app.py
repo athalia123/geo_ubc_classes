956c103b62dc50659fbd6047299487d68c552555
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 18:55:27 +0000

    map update

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,25 +21,40 @@ bcen['lon'] = bcen.geometry.x
 bcen['lat'] = bcen.geometry.y
 bcen1 = bcen.drop('geometry', axis=1)
 
+gclss['lon'] = gclss.geometry.x
+gclss['lat'] = gclss.geometry.y
+gclss1 = gclss.drop('geometry', axis=1)
+
 
 Week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
 #day = 0
 day = st.selectbox("Weekday:", Week, index=0)
+gc2['lon'] = gclss1['lon']
+gc2['lat'] = gclss1['lat']
 gc3 = gc2[gc2["Days"].str.contains(day)].sort_values(by=['Start'])
-st.write(day)
+#st.write(day)
 st.dataframe(gc3)
 
-st.divider()
+#st.divider()
 
 bcen1 = pd.DataFrame(bcen1)
 
 map = folium.Map(location=[49.266048, -123.250012], zoom_start=15)
 folium.GeoJson('geo_files/ubcv_buildings.geojson').add_to(map)
 
+'''
 for i in range(0,len(bcen1)):
    folium.CircleMarker(
       location=[bcen1.iloc[i]['lat'], bcen1.iloc[i]['lon']],
       tooltip=bcen1.iloc[i]['NAME'], radius=3.5, fill=True, color='black', fillcolor='red', fillopacity=1
+   ).add_to(map)
+'''
+
+for i in range(0,len(gc3)):
+   tooltip = gc3.iloc[i]['Section']+'\n'+gc3.iloc[i]['Start']+'\n'+gc3.iloc[i]['SHORTNAME']
+   folium.CircleMarker(
+      location=[gc3.iloc[i]['lat'], gc3.iloc[i]['lon']],
+      tooltip=tooltip, radius=3.5, fill=True, color='black', fillcolor='red', fillopacity=1
    ).add_to(map)
 
 st_map = st_folium(map, width=700, height=450)
