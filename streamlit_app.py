4f76b94f38fea52380a1158b686c79bc518aa8f8
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 17:01:26 +0000

    map geometry again

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.dataframe(gc2)
 st.divider()
 
 bcen = gpd.read_file("geo_files/ubc_buildings_centroids.geojson")
-bcen['lon'] = bcen.bcen['geometry'].x
-bcen['lat'] = bcen.bcen['geometry'].y
+bcen['lon'] = bcen.geometry.x
+bcen['lat'] = bcen.geometry.y
 bcen1 = bcen.drop('geometry', axis=1)
 st.map(bcen1, color="#ffaa0088")
