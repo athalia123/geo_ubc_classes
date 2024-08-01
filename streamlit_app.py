6538ccbc1dfa63173b5e979dd37bddc5c0b7ed37
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 18:17:02 +0000

    map update center

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.divider()
 
 bcen1 = pd.DataFrame(bcen1)
 
-map = folium.Map(location=[49.262948, -123.245912], zoom_start=15)
+map = folium.Map(location=[49.263948, -123.248912], zoom_start=15)
 folium.GeoJson('geo_files/ubcv_buildings.geojson').add_to(map)
 
 for i in range(0,len(bcen1)):
