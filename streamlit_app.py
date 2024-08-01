4ee1cfe659e2945ed3a679f893f91c39e1895d17
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 15:14:39 +0000

    another edit to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.header('My First App - Hello World :)')
+st.title('My First App - Hello World :)')
+st.write("I'm going to make a map of the campus and put my classes on it")
 
