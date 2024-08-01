d046b3db4c26bf08cf93570fd77cf0ae849741b6
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-01 16:02:16 +0000

    add print

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 st.title('My First App - Hello World :)')
 st.write("I'm going to make a map of the campus and put my classes on it")
+print('hello')
 
 gclss = gpd.read_file("geo_files/geoclass1.geojson")
 gc = pd.DataFrame(gclss)
 
 st.table(gc)
-
