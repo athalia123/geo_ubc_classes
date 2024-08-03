996675a556093fdc45d65966fee0c9525d83ebe0
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-03 13:00:09 +0000

    add distance function to classes dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
+from pyproj import CRS
 
 st.set_page_config(page_title='UBCV Class Map', page_icon=':bar_chart:', layout='wide')
 
@@ -20,6 +21,17 @@ st.dataframe(gc2)
 
 st.divider()
 
+def calculate_distance(row, dest_geom, src_col='geometry'):
+    # Calculate the distances (in meters)
+    dist = row[src_col].distance(dest_geom)
+
+    # Convert into kilometers
+    dist_km = dist / 1000
+
+    # Assign the distance to the original data
+    #row[target_col] = dist_km
+    return dist_km
+
 bcen = gpd.read_file("geo_files/ubc_buildings_centroids.geojson")
 bcen['lon'] = bcen.geometry.x
 bcen['lat'] = bcen.geometry.y
@@ -28,6 +40,7 @@ bcen1 = bcen.drop('geometry', axis=1)
 gclss['lon'] = gclss.geometry.x
 gclss['lat'] = gclss.geometry.y
 gclss1 = gclss.drop('geometry', axis=1)
+
 
 st.header('Weekday Schedule Picker')
 st.write('Choose a day -> the class that day will be displayed in order of time AND be shown on the map below it')
@@ -39,7 +52,34 @@ gc2['lon'] = gclss1['lon']
 gc2['lat'] = gclss1['lat']
 gc3 = gc2[gc2["Days"].str.contains(day)].sort_values(by=['Start'])
 #st.write(day)
-st.dataframe(gc3)
+gc4 = gc3
+
+distances = []
+for i in range(0,len(g1)-1):    
+    g2 = g4.iloc[[i]][['geometry', 'lon', 'lat']]
+    ln = g2.iloc[0]['lon']
+    lt = g2.iloc[0]['lat']
+    g3 = g2[['geometry']]
+
+    aeqd = CRS(proj='aeqd', ellps='WGS84', datum='WGS84', lat_0=lt, lon_0=ln).srs
+
+    # Reproject to aeqd projection using Proj4-string
+    g3 = g3.to_crs(crs=aeqd)
+
+    g4 = g4.iloc[[i+1]][['geometry']].to_crs(crs=aeqd)
+
+    g3_geom = g3['geometry'].iloc[0]
+
+    g5 = calculate_distance(g4, dest_geom=g3_geom)
+
+    #print(type(g5))
+    #print(g5)
+    distances.append(g5.iloc[0])
+distances.append('None')
+
+g4['Dist to Next Class'] = distances
+
+st.dataframe(gc4)
 
 #st.divider()
 
