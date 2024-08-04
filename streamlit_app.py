cf416de3dea1c52f70173ed7107e8b7c86ffbad4
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-04 05:06:43 +0000

    map update

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ st.write(crs1)
 bldg2 = bldg.to_crs(epsg=3857)
 st.write(bldg2.crs)
 
-map = folium.Map(location=[49.266048, -123.250012], zoom_start=15, min_zoom=14)
+map = folium.Map(location=[49.266048, -123.250012], zoom_start=15, min_zoom=14, control_scale=True)
 folium.GeoJson(bldg2).add_to(map)
 
 #for i in range(0,len(bcen1)):
