ccef33e2719be090998ae267e45bb9481ba011e0
Athalia Setiawan <56013729+athalia123@users.noreply.github.com>
2024-08-21 17:10:41 +0000

    aggrid test

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,14 +165,16 @@ gc4['Walking duration /min'] = time_list
 
 #st.write("directions: ", directions)
 
-gc4_cols = ["Order","Section", "Instructional Format", "Start", "End", "Building", "Room", "Dist to Next Class /km", "Walking distance /km", "Walking duration /min"]
+gc4_cols = ["Section", "Instructional Format", "Start", "End", "Building", "Room", "Dist to Next Class /km", "Walking distance /km", "Walking duration /min"]
 st.dataframe(gc4[gc4_cols])
-#ngc5 = gc4.copy()
 
 ####################
 ###### AGGRID ######
-
-gb = GridOptionsBuilder.from_dataframe(gc4)
+gc5 = gc4
+gc5["Order"] = l
+gc5_cols = ["Order", "Section", "Instructional Format", "Start", "End", "Building", "Room", "Walking distance /km", "Walking duration /min", "lon", "lat"]
+gc5 = gc5[gc5_cols]
+gb = GridOptionsBuilder.from_dataframe(gc5)
 #make all columns editable
 #gb.configure_columns(cols, editable=True)
 selection_mode='single'
@@ -183,7 +185,7 @@ gb.configure_selection(
 gb.configure_auto_height()
 go = gb.build()
 ag = AgGrid(
-        gc4, 
+        gc5, 
         gridOptions=go, 
         height=200, 
         fit_columns_on_grid_load=True
