e3eace9a73a362445c333869cbadb5859013a870
Athalia Setiawan <athalia.setiawan@gmail.com>
2025-05-07 07:33:01 -0700

    order fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,15 @@ if uploaded_file is not None or sample is True:
         day = st.selectbox("Weekday:", Week, index=0)
         gc2['lon'] = gclss1['lon']
         gc2['lat'] = gclss1['lat']
-        gc3 = gc2[gc2["Days"].str.contains(day)].sort_values(by=['Start'])
+        gc2['End'] = gc2['End'].astype('str')
+        gc2['Start'] = gc2['Start'].astype('str')
+        # print(f"string lenghts of End = {gc2['End'].str.len()}")
+        st.write('string lenghts of End')
+        st.write(gc2['End'].str.len())
+        st.write('string lenghts of start')
+        st.write(gc2['Start'].str.len())
+        # print(f"string lenghts of Start = {gc2['Start'].str.len()}")
+        gc3 = gc2[gc2["Days"].str.contains(day)].sort_values(by=['End'])
         # gc3 is still geopandas Dataframe
         # gc4 is already normal pandas Dataframe
 
